fcf40adb891b950511b4b2eb792d45e6c8c703ab
Penn York <markyan@foxmail.com>
2025-10-30 14:05:16 +0800

    fix: drizzle studio 的问题

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'drizzle-kit'
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
+
+function getLocalD1DB(): string {
+  try {
+    const basePath = resolve('.wrangler/state/v3/d1/miniflare-D1DatabaseObject')
+    const dbFile = readdirSync(basePath, { encoding: 'utf-8' })
+      .find((f) => f.endsWith('.sqlite'))
+
+    if (!dbFile) throw new Error(`.sqlite file not found in ${basePath}`)
+
+    return resolve(basePath, dbFile)
+  } catch (err) {
+    console.error(`Error finding local D1 database: ${(err as Error).message}`)
+    throw err
+  }
+}
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: '.wrangler/state/v3/d1/miniflare-D1DatabaseObject/f5e2dcc5-d467-45d9-b872-114712a4da8e.sqlite',
+    url: getLocalD1DB(),
   },
 })
